dbcaa14f6f74b22970ad36d7fddbb7b6407bf4ac
hayeon song <shy2907@hanmail.net>
2020-06-27 14:47:24 +0900

    First commit

---
+++ startbootstrap-sb-admin-gh-pages/dist/js/scripts.js
@@ -0,0 +1,34 @@
+/*!
+    * Start Bootstrap - SB Admin v6.0.1 (https://startbootstrap.com/templates/sb-admin)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+(function ($) {
+    "use strict";
+
+    $('#call').click(function () {
+        $('#show').html('....loading...');
+
+        $.ajax({
+            type: "GET",
+            url: "https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20171110",
+            success: function (data) {
+                $('#show').html(JSON.stringify(data));
+            }
+        })
+    })
+
+    // Add active state to sidbar nav links
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+        if (this.href === path) {
+            $(this).addClass("active");
+        }
+    });
+
+    // Toggle the side navigation
+    $("#sidebarToggle").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+})(jQuery);
